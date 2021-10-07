63b66c6e20d65401d379eae7155ce9fcc69c2be7
Ant <huertinez.family@outlook.com>
2021-10-07 14:59:43 -0700

    fixed bugs

---
+++ __tests__/animals.test.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+const { 
+    filterByQuery,
+    findById,
+    createNewAnimal,
+    validateAnimal,
+} = require('../lib/anomals.js');
+
+const { animals } = require("../data/animals");
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+
+test("creates an animal object", () => {
+    const animal = createNewAnimal(
+        { name: "Darlene", id: "jhgdja3ng2" },
+        animals
+    );
+
+    expect(animal.name).toBe("Darlene");
+    expect(animal.id).toBe("jhgdja3ng2");
+});
+
+test("filters by query", () => {
+    const startingAnimals = [
+        {
+            id: "3",
+            name: "Erica",
+            species: "gorilla",
+            diet: "omnivore",
+            personalityTraits: ["quirky", "rash"],       
+        },
+        {
+            id: "4",
+            name: "Noel",
+            species: "bear",
+            diet: "carnivore",
+            personalityTraits: ["impish", "sassy", "brave"],
+          },
+    ];
+
+    const updatedAnimals = filterByQuery({ species: "gorilla" }, startingAnimals);
+
+    expect(result.name).toBe("Erica");
+});
+
+test("validates personalitiy traits", () => {
+    const animal = {
+        id: "3",
+        name: "Erica",
+        species: "gorilla",
+        diet: "omnivore",
+        personalityTraits: ["quirky", "rash"],        
+    };
+
+    const invalidAnimal = {
+        id: "3",
+        name: "Erica",
+        species: "gorilla",
+        diet: "omnivore",
+    };
+
+    const result = validateAnimal(animal);
+    const result2 = validateAnimal(invalidAnimal);
+
+    expect(result).toBe(true);
+    expect(result2).toBe(false);
+});
