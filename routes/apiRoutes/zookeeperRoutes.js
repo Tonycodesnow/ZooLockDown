5802db3d902c29141dead1d896a178de1fe69b60
Ant <huertinez.family@outlook.com>
2021-10-10 20:03:33 -0700

    Dataset and Functionality for Zookeepers

---
+++ routes/apiRoutes/zookeeperRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+    filterByQuery,
+    findById,
+    createNewZookeeper,
+    validateZookeeper,
+} = require("../../lib/zookeepers");
+const { zookeepers } = require("../../data/zookeepers");
+
+router.get("/zookeepers", (req, res) => {
+    let results = zookeepers;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+router.post("/zookeepers", (req, res) => {
+    req.body.id = zookeepers.length.toString();
+
+    if (!validateZookeeper(req.body)) {
+        res.status(400).send("The zookeeper is not properly formatted.");
+    } else {
+        const zookeeper = createNewZookeeper(req.body, zookeepers);
+    }
+});
+
+module.exports = router;
